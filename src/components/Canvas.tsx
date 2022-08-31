804dbc054be3572a99389c10a447036b6601d1d9
llallonen <lerushok@ural3000.local>
2022-08-31 22:02:05 +0500

    implement the canvas component and function for clearing the canvas

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 export interface ICanvas {
     height: number;
     width: number;
 }
 
 export const Canvas = ({ height, width }: ICanvas) => {
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
+
+    useEffect(() => {
+        setContext(canvasRef.current && canvasRef.current.getContext('2d'));
+    }, [context])
+
     return (
         <canvas
+            ref={canvasRef}
             style={{
                 border: "3px solid black",
             }}

d19681f7fb7b302971b0dcf85a8beca634fe6943
llallonen <llallonen@gmail.com>
2022-09-16 22:35:20 +0800

    add handling keyboard event fuction and moving snake logic

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { makeMove } from '../store/actions';
 import { IGlobalState } from '../store/reducers';
 import { drawObject, generateRandomPosition, IObjectBody } from '../utilities';
 export interface ICanvas {
@@ -9,14 +10,62 @@ export interface ICanvas {
 
 export const Canvas = ({ height, width }: ICanvas) => {
     const snake1 = useSelector((state: IGlobalState) => state.snake);
-  
+    const disallowedDirection = useSelector(
+        (state: IGlobalState) => state.disallowedDirection
+      );
+
     const [pos, setPos] = useState<IObjectBody>(
-      generateRandomPosition(width - 20, height - 20)
+        generateRandomPosition(width - 20, height - 20)
     );
 
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
     const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
-  
+
+    const handleKeyEvent = useCallback((event: KeyboardEvent) {
+        if (disallowedDirection) {
+            switch (event.key) {
+                case 'w':
+                    moveSnake(0, -20, disallowedDirection);
+                    break;
+                case 's':
+                    moveSnake(0, 20, disallowedDirection);
+                    break;
+                case 'w':
+                    moveSnake(-20, 0, disallowedDirection);
+                    break;
+                case 'w':
+                    event.preventDefault();
+                    moveSnake(-20, 0, disallowedDirection);
+                    break;
+            }
+        } else {
+            if (
+                disallowedDirection !== 'LEFT' &&
+                disallowedDirection !== 'UP' && 
+                disallowedDirection !== 'DOWN' &&
+                event.key === "d"
+            ) {
+                moveSnake(20, 0, disallowedDirection)
+            }
+        },
+        [disallowedDirection, moveSnake]
+    };
+
+    const moveSnake = useCallback((dx = 0, dy = 0, ds: string) => {
+        if (dx < 0 && dy === 0 && ds !== "LEFT") {
+            dispatchEvent(makeMove(dx, dy, "MOVE_LEFT"))
+        }
+        if (dx > 0 && dy === 0 && ds !== "RIGHT") {
+            dispatchEvent(makeMove(dx, dy, "MOVE_RIGHT"))
+        }
+        if (dx === 0 && dy < 0 && ds !== "UP") {
+            dispatchEvent(makeMove(dx, dy, "MOVE_UP"))
+        }
+        if (dx === 0 && dy === 0 && ds !== "DOWN") {
+            dispatchEvent(makeMove(dx, dy, "MOVE_DOWN"))
+        },
+        [dispatch]
+    };
 
     useEffect(() => {
         setContext(canvasRef.current && canvasRef.current.getContext("2d"));
@@ -25,15 +74,24 @@ export const Canvas = ({ height, width }: ICanvas) => {
 
     }, [context])
 
+    useEffect(() => {
+        window.addEventListener('keypress', handleKeyEvent);
+
+        return () => {
+            window.removeEventListener('keypress', handleKeyEvent)
+        }
+
+    }, [disallowedDirection, handleKeyEvent]);
+
     return (
-        <canvas
-            ref={canvasRef}
-            style={{
-                border: "3px solid black",
-            }}
-            height={height}
-            width={width}
-        />
-    );
+    <canvas
+        ref={canvasRef}
+        style={{
+            border: "3px solid black",
+        }}
+        height={height}
+        width={width}
+    />
+);
 };
 
--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -6,9 +6,10 @@ export interface ISnakeCoord {
 export interface IGlobalState {
     snake: ISnakeCoord[] | [];
     disallowedDirection: string;
+    score: number;
 }
 
-const GlobalState: IGlobalState = {
+const globalState: IGlobalState = {
     snake: [
         { x: 580, y: 300 },
         { x: 560, y: 300 },
@@ -17,17 +18,25 @@ const GlobalState: IGlobalState = {
         { x: 500, y: 300 },
     ],
     disallowedDirection: "",
-}
+    score: 0,
+};
 
-
-export const gameReducer = (state = GlobalState, action: any) => {
+export const gameReducer = (state = globalState, action: any) => {
     switch (action.type) {
         case "RIGHT":
         case "LEFT":
         case "UP":
         case "DOWN":
+            let newSnake = [...state.snake];
+            newSnake = [{
+                x: state.snake[0].x + action.payload[0]
+                y: state.snake[0].y + action.payload[1]
+            }, ...newSnake];
+            newSnake.pop();
+
             return {
-                ...state, data: action.payload
+                ...state,
+                snake: newSnake,
             }
         default:
             return state;
